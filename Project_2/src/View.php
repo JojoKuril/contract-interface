dcf3722d2d85b0c89d636c9490330dcf23b72171
JojoKuril <91073420+JojoKuril@users.noreply.github.com>
2021-09-24 10:27:31 +0400

    Update View.php

--- Project_2/src/View.php
+++ Project_2/src/View.php
@@ -3,10 +3,7 @@
 class View 
 {
     public static function render($template, $vars =[]) {
-        //var['title'] = 'Заголовок';
         extract($vars);
-
-        //echo $title ;
         
         require __DIR__ . '/views/' .  $template . '.php';
     }
